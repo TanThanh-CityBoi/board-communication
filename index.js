ab5589ac1559a3ba856d672828b6fefe22facfdf
TanThanh-CityBoi <tanthanhe@gmail.com>
2024-05-30 16:35:08 +0700

    feat: nodemon

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-const express = require('express');
-const app = express();
-
-
-const morgan = require('morgan');
-const cors = require('cors');
-const bp = require('body-parser');
-require("dotenv").config();
-
-
-app.use(bp.json())
-app.use(bp.urlencoded({ extended: true }))
-
-app.use(morgan('combined'));
-app.use(cors());
-
 
 const SerialPort = require('serialport').SerialPort;
 const sp = new SerialPort({path: "COM4", baudRate: 115200});
@@ -37,26 +21,24 @@ sp.on("open", function(err) {
 });
 
 sp.on('data', function(data){
-    console.log("🚀 ~ sp.on ~ data:", data)
-    // decoding uint8Array to string
+    console.log("🚀 ~ SP.ON_DATA:", data)
+
+    //
     const enc = new TextDecoder();
-    console.log("🚀 ~ sp.on ~ enc:", enc)
     const arr = new Uint8Array(data);
-    console.log("🚀 ~ sp.on ~ arr:", arr)
+    console.log("🚀 ~ UNIT_ARR:", arr)
+
     const parsedData = enc.decode(arr)
+    console.log("🚀 ~ PARSED_DATA:", arr)
 
 
     sp.write(`COM4_REPLY: ${data}`, () => {
-        console.log("com4 returnn")
+        console.log("COM4_RETURNED")
     })
 
-    console.log('Data received: ', parsedData);
+    console.log('DATA_RECEIVED: ', parsedData);
 });
 
 
 
 
-//app listen
-app.listen(5000, () => {
-    console.log(`Server is running on port 5000`);
-})
